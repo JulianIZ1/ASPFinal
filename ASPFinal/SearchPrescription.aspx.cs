ebad3ccce1ae3f9c9191e13fc4007346dafc5922
JulianIZ1 <59848304+JulianIZ1@users.noreply.github.com>
2020-01-25 14:38:44 -0800

    fdsahjg juhadfskjbvfkl;szdfjlm;k

--- ASPFinal/SearchPrescription.aspx.cs
+++ ASPFinal/SearchPrescription.aspx.cs
@@ -13,7 +13,6 @@ namespace ASPFinal
         {
 
         }
-<<<<<<< HEAD
 
         protected void Delete_Click(object sender, EventArgs e)
         {
@@ -24,59 +23,5 @@ namespace ASPFinal
         {
 
         }
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-
-        protected void btnSearch_Click(object sender, EventArgs e)
-        {
-            if ((txtPrescriptionID.Text.Trim() != "") && (txtMedicationName.Text.Trim() != "") && (txtPhysicianID.Text.Trim() != "") && (txtPatientID.Text.Trim() != ""))
-            {
-                try
-                {
-                    Cache.Remove("PrescriptionData");
-                    DataBind();
-                }
-                catch (Exception Ex)
-                {
-                    throw new Exception(Ex.Message, Ex.InnerException);
-                }
-            }
-        }
-        private void DataBind()
-        {
-            PaitentDataTier aDatatier = new PaitentDataTier();
-            ViewState["vPrescriptionID"] = txtPrescriptionID.Text.Trim();
-            ViewState["vMedicationName"] = txtMedicationName.Text.Trim();
-            ViewState["vPhysicianID"] = txtPhysicianID.Text.Trim();
-            ViewState["vPatientID"] = txtPatientID.Text.Trim();
-
-
-
-            DataSet aDataSet = new DataSet();
-            aDataSet = aDatatier.GetPrescription(Convert.ToString(ViewState["vPrescriptionID"]),
-                Convert.ToString(ViewState["vMedicationName"]), Convert.ToString(ViewState["vPhysicianID"]),
-                Convert.ToString(ViewState["vPatientID"]))
-                ;
-
-            grdPrescription.DataSource = aDataSet.Tables[0];
-
-            // Cache for a while
-            if (Cache["CustomerData"] != null)
-            {
-                Cache.Add("PrescriptionData", new DataView(aDataSet.Tables[0]),
-                    null, System.Web.Caching.Cache.NoAbsoluteExpiration, System.TimeSpan.FromMinutes(10),
-                    System.Web.Caching.CacheItemPriority.Default, null);
-            }
-            grdPrescription.DataBind();
-        }
-=======
->>>>>>> parent of 50d7ba7... Dashboard tabs are running
-=======
->>>>>>> parent of a6653a9... datadbind for search patient
-=======
->>>>>>> parent of a6653a9... datadbind for search patient
-=======
->>>>>>> parent of a6653a9... datadbind for search patient
     }
 }
