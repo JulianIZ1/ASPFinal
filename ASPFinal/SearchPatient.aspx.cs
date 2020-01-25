72ffa81994043a560c4d0242080cc115361c3675
JulianIZ1 <59848304+JulianIZ1@users.noreply.github.com>
2020-01-25 14:32:53 -0800

    FUCKJ

--- ASPFinal/SearchPatient.aspx.cs
+++ ASPFinal/SearchPatient.aspx.cs
@@ -17,7 +17,6 @@ namespace ASPFinal
                 // Do end it
             }
         }
-<<<<<<< HEAD
 
         protected void Delete_Click(object sender, EventArgs e)
         {
@@ -31,52 +30,7 @@ namespace ASPFinal
 
         protected void btnSearch_Click(object sender, EventArgs e)
         {
-            if ((txtPatientID.Text.Trim() != "") && (txtFName.Text.Trim() != "") && (txtLName.Text.Trim() != "") && (txtMidInit.Text.Trim() !=""))
-            {
-                try
-                {
-                    Cache.Remove("PaitentData");
-                    DataBind();
-                }
-                catch (Exception Ex)
-                {
-                    throw new Exception(Ex.Message, Ex.InnerException);
-                }
-            }
+
         }
-        private void DataBind()
-        {
-            PaitentDataTier aDatatier = new PaitentDataTier();
-            ViewState["vPatientID"] = txtPatientID.Text.Trim();
-            ViewState["vFName"] = txtFName.Text.Trim();
-            ViewState["vMidInit"] = txtMidInit.Text.Trim();
-            ViewState["vLName"] = txtLName.Text.Trim();
-            ViewState["vgender"] = ddlGender.Text.Trim();
-
-
-
-            DataSet aDataSet = new DataSet();
-            aDataSet = aDatatier.GetPaitent(Convert.ToString(ViewState["vPatientID"]),
-                Convert.ToString(ViewState["vFName"]), Convert.ToString(ViewState["vMidInit"]),
-                Convert.ToString(ViewState["vLName"]), Convert.ToString(ViewState["vgender"]),
-                Convert.ToString(ViewState["vstreet"]), Convert.ToString(ViewState["vCity"]),
-                Convert.ToString(ViewState["vstate"]), Convert.ToString(ViewState["vzip"]),
-                Convert.ToString(ViewState["vdob"]), Convert.ToString(ViewState["vphone1"]),
-                Convert.ToString(ViewState["vphone2"]), Convert.ToString(ViewState["vemail"]))
-                ;
-
-            grdPatient.DataSource = aDataSet.Tables[0];
-
-            // Cache for a while
-            if (Cache["CustomerData"] != null)
-            {
-                Cache.Add("PaitentData", new DataView(aDataSet.Tables[0]),
-                    null, System.Web.Caching.Cache.NoAbsoluteExpiration, System.TimeSpan.FromMinutes(10),
-                    System.Web.Caching.CacheItemPriority.Default, null);
-            }
-            grdPatient.DataBind();
-        }
-=======
->>>>>>> parent of 50d7ba7... Dashboard tabs are running
     }
 }
