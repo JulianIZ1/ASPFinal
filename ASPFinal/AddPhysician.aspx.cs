bb3d28898fee2d748b15fe0daf0b21d299007568
George DeTore <gndetore181@stevenscollege.edu>
2020-01-26 15:20:06 -0500

    meep

--- ASPFinal/AddPhysician.aspx.cs
+++ ASPFinal/AddPhysician.aspx.cs
@@ -9,9 +9,42 @@ namespace ASPFinal
 {
     public partial class AddPhysician : System.Web.UI.Page
     {
+        // Global Varibales
+        string fname, midint, lname, gender, address, city, state, zip, home, cell, email, dt;
+
         protected void Page_Load(object sender, EventArgs e)
         {
+            dt = DateTime.Now.ToString("MM/dd/yyyy");
+            txtDOB.Text = dt;
 
+            ddlState.DataSource = StateManager.getStates();
+            ddlState.DataTextField = "FullAndAbbrev";
+            ddlState.DataValueField = "abbreviation";
+            ddlState.SelectedValue = "PA";
+            ddlState.DataBind();
+        }
+
+        protected void btnHidden_Click(object sender, EventArgs e)
+        {
+            string reply;
+            // Adds into SQL database
+            try
+            {
+                PhysicianDataTier.AddPhysician(txtFName.Text, txtMidInit.Text, txtLName.Text, ddlGender.Text, txtStreet.Text,
+                    txtCity.Text, ddlState.Text, decimal.Parse(txtZIP.Text), DateTime.Parse(txtDOB.Text), txtOfficePhone.Text,
+                    txtPersonalPhone.Text, txtEmailI.Text, txtWorkEmail.Text, txtPosition.Text, txtSpecialty.Text, decimal.Parse(txtSalary.Text));
+
+                reply = "Success";
+                lblDisplay.Text = reply;
+            }
+            catch (Exception Ex)
+            {
+                throw new Exception(Ex.Message);
+            }
+            finally
+            {
+
+            }
         }
     }
 }
--- ASPFinal/DataTiers/PhysicianDataTier.cs
+++ ASPFinal/DataTiers/PhysicianDataTier.cs
@@ -16,45 +16,48 @@ namespace ASPFinal
         static System.Data.SqlClient.SqlCommand cmdString = new System.Data.SqlClient.SqlCommand();
 
 
-        public DataSet AddPhysician(string phyid, string fname, string midint, string lname, string v, string dateTime, string gender, string streetname, string city, string zip, string docstate, string officephone, string personalphone, string workemail, string personalemail, string speciality, string salary)
+        public DataSet AddPhysician(string phyid, string fname, string midint, string lname, DateTime dob, string gender, string streetname,
+            string city, decimal zip, string docstate, string officephone, string personalphone, string workemail, string personalemail, string speciality, decimal salary)
         {
 
             try
             {
+                // Open Connection
                 myConn.Open();
-                SqlCommand command = new SqlCommand("dbo.Add_Physician", myConn);
 
-                command.Parameters.Clear();
+                // Clears Commmand
+                cmdString.Parameters.Clear();
 
+                // Command
+                cmdString.Connection = myConn;
+                cmdString.CommandType = CommandType.StoredProcedure;
+                cmdString.CommandTimeout = 1500;
+                cmdString.CommandText = "Find_physician";  //name of stored procedure
 
-                command.CommandType = CommandType.StoredProcedure;
-                command.CommandTimeout = 1500;
-                command.CommandText = "Add_Physician";
-
-                cmdString.Parameters.Add("@physician_id", SqlDbType.Int).Value = phyid;    //parameter names must match and in same
+                //Define input parameter
                 cmdString.Parameters.Add("@fname", SqlDbType.VarChar, 25).Value = fname;
                 cmdString.Parameters.Add("@midint", SqlDbType.VarChar, 1).Value = midint;
                 cmdString.Parameters.Add("@lname", SqlDbType.VarChar, 25).Value = lname;
-                cmdString.Parameters.Add("@dob", SqlDbType.Date).Value = dateTime;
+                cmdString.Parameters.Add("@dob", SqlDbType.DateTime2, 7).Value = dob;
                 cmdString.Parameters.Add("@gender", SqlDbType.Char, 6).Value = gender;
                 cmdString.Parameters.Add("@streetname", SqlDbType.VarChar, 40).Value = streetname;
                 cmdString.Parameters.Add("@city", SqlDbType.VarChar, 40).Value = city;
                 cmdString.Parameters.Add("@doc_state", SqlDbType.Char, 2).Value = docstate;
-                cmdString.Parameters.Add("@zip", SqlDbType.Decimal, 5).Value = zip;
+                cmdString.Parameters.Add("@zip", SqlDbType.Decimal).Value = zip;
                 cmdString.Parameters.Add("@office_phone", SqlDbType.NChar, 14).Value = officephone;
                 cmdString.Parameters.Add("@personal_phone", SqlDbType.NChar, 14).Value = personalphone;
                 cmdString.Parameters.Add("@work_email", SqlDbType.VarChar, 40).Value = workemail;
                 cmdString.Parameters.Add("@personal_email", SqlDbType.VarChar, 40).Value = personalemail;
                 cmdString.Parameters.Add("@speciality", SqlDbType.VarChar, 40).Value = speciality;
-                cmdString.Parameters.Add("@salary", SqlDbType.Decimal, 8).Value = salary;
+                cmdString.Parameters.Add("@salary", SqlDbType.Decimal).Value = salary;
 
-                SqlDataAdapter aAdapter = new SqlDataAdapter();
-                command.ExecuteNonQuery();
-
+                // Adapter and Dataset
+                SqlDataAdapter adapter = new SqlDataAdapter();
+                adapter.SelectCommand = cmdString;
+                cmdString.ExecuteNonQuery();
                 return null;
-
             }
-            catch(Exception Ex)
+            catch (Exception Ex)
             {
                 throw new Exception(Ex.Message);
             }
