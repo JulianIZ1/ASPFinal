54e91c8b9ac156a626b69b234de5b40f7b8fc456
George DeTore <gndetore181@stevenscollege.edu>
2020-01-29 16:45:11 -0500

    yahhha

--- ASPFinal/DataTiers/PrescriptionDataTier.cs
+++ ASPFinal/DataTiers/PrescriptionDataTier.cs
@@ -60,6 +60,52 @@ namespace ASPFinal.DataTiers
                 myConn.Close();
             }
         }
+
+
+        public static void UpdatePrescription(string rxnum, string medic, decimal amt, DateTime refill_date, string dosage, string intake,
+                                        string freq, int patid, int phyid)
+        {
+            try
+            {
+                // Open Connection
+                myConn.Open();
+
+                // Clears Commmand
+                cmdString.Parameters.Clear();
+
+                // Command
+                cmdString.Connection = myConn;
+                cmdString.CommandType = CommandType.StoredProcedure;
+                cmdString.CommandTimeout = 1500;
+                cmdString.CommandText = "Update_Prescription";
+
+                // Parameters
+                cmdString.Parameters.Add("@rx_number", SqlDbType.Char, 3).Value = rxnum;
+                cmdString.Parameters.Add("@medication_name", SqlDbType.VarChar, 50).Value = medic;
+                cmdString.Parameters.Add("@refill_amt", SqlDbType.Decimal).Value = amt;
+                cmdString.Parameters.Add("@refill_date", SqlDbType.DateTime2, 7).Value = refill_date;
+                cmdString.Parameters.Add("@dosage", SqlDbType.Char, 3).Value = dosage;
+                cmdString.Parameters.Add("@intake_method", SqlDbType.VarChar, 250).Value = intake;
+                cmdString.Parameters.Add("@frequency", SqlDbType.VarChar, 50).Value = freq;
+                cmdString.Parameters.Add("@patient_id", SqlDbType.Int).Value = patid;
+                cmdString.Parameters.Add("@physician_id", SqlDbType.Int).Value = phyid;
+
+                // Adapter and Dataset
+                SqlDataAdapter adapter = new SqlDataAdapter();
+                adapter.SelectCommand = cmdString;
+                cmdString.ExecuteNonQuery();
+              //  return null;
+            }
+            catch (Exception Ex)
+            {
+                throw new Exception(Ex.Message);
+            }
+            finally
+            {
+                myConn.Close();
+            }
+        }
+
         public DataSet ViewPrescription(string precid)
         {
             try
